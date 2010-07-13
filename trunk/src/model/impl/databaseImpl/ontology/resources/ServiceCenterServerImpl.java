c2d873be83477bf1cacca2cbe50cf7b02a84a40f
moldovanus <moldovanus@09bf06d9-9520-a2de-eaa8-0b032701be40>
2010-07-13 07:28:25 +0000

    Failed commit: Default

--- trunk/src/model/impl/databaseImpl/ontology/resources/ServiceCenterServerImpl.java
+++ trunk/src/model/impl/databaseImpl/ontology/resources/ServiceCenterServerImpl.java
@@ -1,13 +1,13 @@
 package model.impl.databaseImpl.resources;
 
-import model.interfaces.resources.ExternalStorage;
+import model.interfaces.resources.ServiceCenterServer;
 
 /**
  * Created by IntelliJ IDEA.
  * User: Administrator
  * Date: Jul 8, 2010
- * Time: 12:36:41 PM
+ * Time: 12:30:21 PM
  * To change this template use File | Settings | File Templates.
  */
-public class ExternalStorageImpl extends ComplexResourceImpl implements ExternalStorage {
+public class ServiceCenterServerImpl extends ComplexHeterogeneousResourcesImpl implements ServiceCenterServer {
 }
