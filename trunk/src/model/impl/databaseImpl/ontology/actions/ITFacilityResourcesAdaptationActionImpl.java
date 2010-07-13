2689e4a6be4076ed71a4b3ddc8e4dbe51e7de5af
moldovanus <moldovanus@09bf06d9-9520-a2de-eaa8-0b032701be40>
2010-07-13 07:30:56 +0000

    Failed commit: Default

---
+++ trunk/src/model/impl/databaseImpl/ontology/actions/ITFacilityResourcesAdaptationActionImpl.java
@@ -0,0 +1,30 @@
+package model.impl.databaseImpl.actions;
+
+import model.interfaces.actions.ContextAction;
+import model.interfaces.actions.ITFacilityResourceAdaptationAction;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: Jul 9, 2010
+ * Time: 9:24:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ITFacilityResourcesAdaptationActionImpl extends ContextActionImpl implements ITFacilityResourceAdaptationAction {
+    private ContextAction action;
+
+    public ITFacilityResourcesAdaptationActionImpl() {
+    }
+
+    public ITFacilityResourcesAdaptationActionImpl(ContextAction action) {
+        this.action = action;
+    }
+
+    public ContextAction getAction() {
+        return action;
+    }
+
+    public void setAction(ContextAction action) {
+        this.action = action;
+    }
+}
