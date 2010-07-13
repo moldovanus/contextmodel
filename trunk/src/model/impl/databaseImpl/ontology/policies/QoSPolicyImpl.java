562e18130eef9126a9d9ee2d64672adf7342dc86
moldovanus <moldovanus@09bf06d9-9520-a2de-eaa8-0b032701be40>
2010-07-13 07:38:22 +0000

    Failed commit: Default

---
+++ trunk/src/model/impl/databaseImpl/ontology/policies/QoSPolicyImpl.java
@@ -0,0 +1,13 @@
+package model.impl.databaseImpl.policies;
+
+import model.interfaces.policies.QoSPolicy;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: Jul 9, 2010
+ * Time: 9:39:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QoSPolicyImpl extends BusinessPolicyImpl implements QoSPolicy {
+}
