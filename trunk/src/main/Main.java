a4eec14cc58cd2a63dc672c0115e1c3c07817a43
moldovanus <moldovanus@09bf06d9-9520-a2de-eaa8-0b032701be40>
2010-07-22 08:57:29 +0000

    de necomit

--- trunk/src/main/Main.java
+++ trunk/src/main/Main.java
@@ -2,19 +2,37 @@ package main;
 
 
 import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.reasoner.Reasoner;
 import edu.stanford.smi.protege.exception.OntologyLoadException;
 import edu.stanford.smi.protegex.owl.ProtegeOWL;
 import edu.stanford.smi.protegex.owl.jena.JenaOWLModel;
+import edu.stanford.smi.protegex.owl.swrl.SWRLRuleEngine;
+import edu.stanford.smi.protegex.owl.swrl.SWRLRuleEngineFactory;
+import edu.stanford.smi.protegex.owl.swrl.bridge.BridgeFactory;
+import edu.stanford.smi.protegex.owl.swrl.bridge.SWRLRuleEngineBridge;
+import edu.stanford.smi.protegex.owl.swrl.bridge.exceptions.SWRLRuleEngineBridgeException;
+import edu.stanford.smi.protegex.owl.swrl.bridge.jess.SWRLJessBridge;
+import edu.stanford.smi.protegex.owl.swrl.exceptions.SWRLRuleEngineException;
+import edu.stanford.smi.protegex.owl.swrl.model.SWRLFactory;
+import edu.stanford.smi.protegex.owl.swrl.model.SWRLImp;
+import edu.stanford.smi.protegex.owl.swrl.parser.SWRLParseException;
 import model.impl.ontologyImpl.OntologyModelFactory;
+import model.interfaces.policies.BusinessPolicy;
+import model.interfaces.policies.ITComputingContextPolicy;
 import model.interfaces.resources.Actuator;
+import model.interfaces.resources.ComplexResource;
+import model.interfaces.resources.ServiceCenterITComputingResource;
+import model.interfaces.resources.SimpleResource;
+import model.interfaces.resources.applications.ApplicationActivity;
 import org.mindswap.pellet.jena.PelletReasonerFactory;
 
 import java.io.File;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,13 +42,18 @@ import java.util.Iterator;
  * To change this template use File | Settings | File Templates.
  */
 public class Main {
+    static JenaOWLModel owlModel;
+    static OntModel ontModel;
+    static OntologyModelFactory protegeFactory;
+
     public static void main(String args[]) {
 
-        File ontologyDataCenterFile = new File("ontology/context.owl");
-        JenaOWLModel owlModel = null;
+        File ontologyDataCenterFile = new File(OntologyModelFactory.ontologyFile);
+        protegeFactory = null;
+        owlModel = null;
         try {
             owlModel = ProtegeOWL.createJenaOWLModelFromURI(ontologyDataCenterFile.toURI().toString());
-            OntologyModelFactory protegeFactory = new OntologyModelFactory(owlModel);
+            protegeFactory = new OntologyModelFactory();
             Collection<Actuator> actuators = protegeFactory.getAllActuatorInstances();
             Iterator<Actuator> currentActuator = actuators.iterator();
             boolean over = false;
@@ -40,16 +63,106 @@ public class Main {
                     ac = currentActuator.next();
                     System.out.println("Actuator 1" + ac.getResourceID());
                 } else over = true;
-
             }
 
         } catch (OntologyLoadException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        OntModel policyConversionModel = ModelFactory.createOntologyModel(PelletReasonerFactory.THE_SPEC);
-        policyConversionModel.add(owlModel.getJenaModel());
+        /***************************Pellet Reasoner*****************************/
+        ontModel = ModelFactory.createOntologyModel(PelletReasonerFactory.THE_SPEC);
+        ontModel.add(owlModel.getJenaModel());
         Reasoner reasoner = PelletReasonerFactory.theInstance().create();
         Model emptyModel = ModelFactory.createDefaultModel();
+        InfModel model = ModelFactory.createInfModel(reasoner, emptyModel);
+        model.add(owlModel.getJenaModel());
+
+
+        /********************** Rete bridge ******************************/
+        jess.Rete testRete = new jess.Rete();
+        try {
+            SWRLJessBridge bridge = new SWRLJessBridge(owlModel, testRete);
+        } catch (SWRLRuleEngineBridgeException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        /*********************************** Print all individuals ******************************/
+//        Collection classes = owlModel.getUserDefinedOWLNamedClasses();
+//        for (Iterator it = classes.iterator(); it.hasNext();) {
+//            OWLNamedClass cls = (OWLNamedClass) it.next();
+//            Collection instances = cls.getInstances(false);
+//            System.out.println("Class " + cls.getBrowserText() + "(" + instances.size() + ")");
+//            for (Iterator jt = instances.iterator(); jt.hasNext();) {
+//                OWLIndividual individual = (OWLIndividual) jt.next();
+//                System.out.println(" - " + individual.getBrowserText());
+//            }
+//        }
+        /***************** Protege SWRL  - add policies + infer ******************/
+        Collection<ITComputingContextPolicy> energyPolicies = protegeFactory.getAllITComputingContextPolicyInstances();
+        SWRLFactory factory = new SWRLFactory(owlModel);
+        SWRLRuleEngineBridge bridge = null;
+        try {
+            bridge = BridgeFactory.createBridge(owlModel);
+        } catch (SWRLRuleEngineBridgeException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        SWRLRuleEngine ruleEngine = null;
+        try {
+            ruleEngine = SWRLRuleEngineFactory.create(owlModel);
+            ruleEngine.run();
+            ruleEngine.reset();
+        } catch (SWRLRuleEngineException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        String swrlRule = "";
+        for (Iterator it = energyPolicies.iterator(); it.hasNext();) {
+            try {
+                ITComputingContextPolicy currentPolicy = (ITComputingContextPolicy) it.next();
+                ComplexResource compResource = (ComplexResource) currentPolicy.getPolicySubject().get(0);
+                swrlRule = "";
+                List<ServiceCenterITComputingResource> assocResource = compResource.getResources();
+                int a = 0;
+                for (Iterator simpleIter = assocResource.iterator(); simpleIter.hasNext();) {
+                    SimpleResource simpleResource = (SimpleResource) simpleIter.next();
+                    swrlRule += "ContextModel:SimpleResource(?a" + a + ") ^ ContextModel:resourceID(?a" + a + ",\"" + simpleResource.getResourceID() + "\") ^ ContextModel:currentWorkload(?a" + a + ",?currentWorkload" + a + ") ^ ContextModel:maximumWorkload(?a" + a + ",?maxWorkload" + a + ") ^ swrlb:lessThanOrEqual(?currentWorkload" + a + ",?maximumWorkload" + a + ")^ swrlb:multiply(?optimalWorkload0,0.33,?downDif" + a + ") ^  \n" +
+                            "sqwrl:difference(?maxWorkload" + a + ", ?optimalWorkload" + a + ",?sumOf" + a + ")  ^  " +
+                            "sqwrl:difference(?optimalWorkload" + a + ",?downDif" + a + ",?minThreshold" + a + ")  ^  " +
+                            " swrlb:divide(?sumOf" + a + ", 2,?upDif" + a + ") ^ \n" +
+                            "sqwrl:sum(?optimalWorkload" + a + ",?upDif" + a + ",?maxThreshold" + a + " )   ^ swrlb:lessThanOrEqual(?minThreshold" + a + ",?currentWorkload" + a + ")^ swrlb:lessThanOrEqual(?currentWorkload" + a + ",?maxThreshold" + a + ") ^ ";
+                    a++;
+                }
+                System.out.println(swrlRule + "ContextModel:ComplexResource(?x) ^ ContextModel:ResourceID(?x," + compResource.getResourceID() + ") ^  ContextModel:currentWorkload(?x, ?currentWorkload) ^  ContextModel:maximumWorkload(?x, ?maxWorkload) ^  swrlb:lessThanOrEqual(?currentWorkload, ?maximumWorkload) -> isRespected(ContextModel:" + currentPolicy.getPolicyName() + ", true)");
+                SWRLImp imp = factory.createImp(swrlRule + "ContextModel:ComplexResource(?x) ^  ContextModel:currentWorkload(?x, ?currentWorkload) ^  ContextModel:maximumWorkload(?x, ?maxWorkload) ^  swrlb:lessThanOrEqual(?currentWorkload, ?maximumWorkload) -> ContextModel:isRespected(ContextModel:" + currentPolicy.getPolicyName() + ", true)");
+
+            } catch (Exception e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+        Collection<BusinessPolicy> businessPolicies = protegeFactory.getAllBusinessPolicyInstances();
+        for (Iterator it = businessPolicies.iterator(); it.hasNext();) {
+            BusinessPolicy businessPolicy = (BusinessPolicy) it.next();
+            ApplicationActivity applicationActivity = (ApplicationActivity) businessPolicy.getPolicySubject().get(0);
+            try {
+
+                swrlRule += "ContextModel:ApplicationActivity(?x) ^ ContextModel:resourceID(?x,\"" + applicationActivity.getResourceID() + "\") ^ ContextModel:memAllocatedValue(?x,?memAllocated) ^ ContextModel:memRequiredValue(?x, ?memRequiredValue) ^ swrlb:lessThanOrEqual(?memAllocatedValue,?memRequiredValue) \n " +
+                        " ^ ContextModel:hddAllocatedValue(?x,?hddAllocated) ^ ContextModel:hddRequiredValue(?x, ?hddRequiredValue) ^ swrlb:lessThanOrEqual(?hddAllocatedValue,?hddRequiredValue)  \n" +
+                        " ^ ContextModel:cpuAllocatedValue(?x,?cpuAllocated) ^ ContextModel:cpuRequiredValue(?x, ?cpuRequiredValue) ^ swrlb:lessThanOrEqual(?cpuAllocatedValue,?cpuRequiredValue) ";
+                System.out.println(swrlRule);
+                SWRLImp imp = factory.createImp(swrlRule);
+
+            } catch (SWRLParseException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+
+
+        /************** Protege reasoner does not work **************/
+//        try {
+//            ruleEngine.infer();
+//        } catch (Exception e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        }
+
 
         /*
 Configuration config =
@@ -111,5 +224,20 @@ System.out.println(o.toString());
 //
 
 
+    }
+
+    public static void printIterator(Iterator<?> i, String header) {
+        System.out.println(header);
+        for (int c = 0; c < header.length(); c++)
+            System.out.print("=");
+        System.out.println();
+
+        if (i.hasNext()) {
+            while (i.hasNext())
+                System.out.println(i.next());
+        } else
+            System.out.println("<EMPTY>");
+
+        System.out.println();
     }
 }
