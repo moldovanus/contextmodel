4e9c68f26c39375d4e84a8362d36395ef4e21116
Moldovanus <Moldovanus@09bf06d9-9520-a2de-eaa8-0b032701be40>
2010-10-04 12:33:11 +0000

    pus JTree for decision

--- trunk/src/utils/clustering/Cluster.java
+++ trunk/src/utils/clustering/Cluster.java
@@ -10,8 +10,10 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public interface Cluster {
+    public final int INFINITY= 100000;
     public void addToCluster (Object o);
     public void removeFromCluster (Object o);
     public void refreshClusterCenter();
     public List getAllElements();
+    public double distanceToCluster(Object o);
 }
